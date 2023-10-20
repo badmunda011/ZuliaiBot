fbcef22297f729fbc988c83d182b5fd07f09f748
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-20 15:33:04 +0530

    Update chatgpt.py

--- Zuli/modules/chatgpt.py
+++ Zuli/modules/chatgpt.py
@@ -12,24 +12,31 @@ openai.api_key = config.GPT_API
 
 
 
+@Zuli.on_message(filters.command(["chatgpt", "ai", "ask"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+async def chatgpt(zuli, message):
+    name = message.from_user.first_name
 
-@Zuli.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-async def chat(zuli :Zuli, message):
-  name = message.from_user.first_name
-    
     try:
         await zuli.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
-            await message.reply(f"Hello {name}\nPlease provide text after the .ai command ")
+            await message.reply(f"Hello {name}\nPlease provide text after the .ai command")
         else:
             a = message.text.split(' ', 1)[1]
             MODEL = "gpt-3.5-turbo"
-            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
-    temperature=0.2)
-            x=resp['choices'][0]["message"]["content"]
-            await message.reply_text(f"{x}")     
+            resp = openai.ChatCompletion.create(
+                model=MODEL,
+                messages=[{"role": "user", "content": a}],
+                temperature=0.2
+            )
+            x = resp['choices'][0]["message"]["content"]
+            await message.reply_text(f"{x}")
     except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")        
+        await message.reply_text(f"**Error**: {e}")
+
+
+
+
+                                    
 
 
 
