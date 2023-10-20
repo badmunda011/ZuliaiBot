cceefc6baf7ff2c0cc23ebec4374ea3acb02ce14
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-20 15:07:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from os import getenv
+
+
+API_ID = int(getenv("API_ID", "26850449"))
+API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+GPT_API = getenv("GPT_API", "")
+DEEP_API = getenv("DEEP_API", "")
