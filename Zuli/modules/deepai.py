9480c029892b978f5c580a050cd3aca007b9a2fd
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-20 14:57:39 +0530

    Create deepai.py

---
+++ Zuli/modules/deepai.py
@@ -0,0 +1,38 @@
+import requests
+from pyrogram import filters
+from Zuli import Zuli
+from pyrogram.enums import ChatAction, ParseMode
+
+api_key = ""
+
+
+
+@Zuli.on_message(filters.command(["deep"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(zuli: Zuli, message):
+  name = message.from_user.first_name
+    try:
+        await zuli.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply("Hello {name\n}Please provide text after the /deep command")
+                
+        else:
+            a = message.text.split(' ', 1)[1]
+
+            data = {
+                'text': a,  
+            }
+
+            headers = {
+                'api-key': api_key,
+            }
+
+            r = requests.post("https://api.deepai.org/api/text-generator", data=data, headers=headers)
+            response = r.json()
+            answer_text = response['output']
+            await message.reply_text(f"{answer_text}")
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ**: {e} ")
+
+
+
+
