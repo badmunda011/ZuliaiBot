5e9fee6095776054fb8524da478a0d1ca5e593bb
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-20 15:17:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-GPT_API = getenv("GPT_API", "")
-DEEP_API = getenv("DEEP_API", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "6796545941:AAFr4JMYPbOTRtt1mTJ9vG1MPo9h8jtTmHA")
+OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
+GPT_API = getenv("GPT_API", "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")
+DEEP_API = getenv("DEEP_API", "9133bea4-ddf1-40d0-bcac-089e0fbacb4f")
