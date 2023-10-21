0e616a43840b1698f1a42d730c9e11a356235318
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-10-21 13:19:11 +0530

    Create zuli.py

---
+++ Zuli/modules/zuli.py
@@ -0,0 +1,20 @@
+from lexica import Client
+from pyrogram import filters
+from Zuli import Zuli
+from pyrogram.types import ChatAction
+
+def main(prompt: str) -> str:
+    client = Client()
+    response = client.palm(prompt)
+    return response["content"].strip()
+
+@Zuli.on_message(filters.regex(r"zuli|Zuli"))
+async def deepchat(zuli: Zuli, message):
+    if message.reply_to_message:
+        query = message.text.split(' ', 1)[1]
+        x = main(query)
+        await message.reply(x)
+    else:
+        query = message.text.split(' ', 1)[1]
+        x = main(query)
+        await message.reply(x)
