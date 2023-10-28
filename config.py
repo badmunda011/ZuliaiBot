15d634e81a53a6f5a9dd9c32316281f9a3e17745
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-28 13:54:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6796545941:AAE6FhGRk6lqtfoACEO4LpJXhRZPi5RVTeQ"
 BOT_USERNAME = getenv("BOT_USERNAME", "ZuliaiBot")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
 GPT_API = getenv("GPT_API", "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")
-DEEP_API = getenv("DEEP_API", "9133bea4-ddf1-40d0-bcac-089e0fbacb4f")
+DEEP_API = getenv("DEEP_API", "c8e3d7fc-1f7e-455b-8019-5c1b7f21047a")
