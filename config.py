9d9867c08106ca130756327a79c464be40d889b3
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-02 23:42:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "6796545941:AAE6FhGRk6lqtfoACEO4LpJXhRZPi5RVTeQ")
+POKI_TOKEN = getenv("BOT_TOKEN", "6763397502:AAGwKKYxTucW2POx6MYY_275FFo_y8oD_7k")
 BOT_USERNAME = getenv("BOT_USERNAME", "ZuliaiBot")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
 GPT_API = getenv("GPT_API", "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")
