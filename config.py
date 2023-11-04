200323666ce850b29984d9a085d430ede384a947
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-04 14:07:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6196153811:AAGdVbn8tc6S2znNcOb7tWWoTxFzzZXbycE"
 POKI_TOKEN = getenv("POKI_TOKEN", "6763397502:AAGwKKYxTucW2POx6MYY_275FFo_y8oD_7k")
 BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
+GPT_API = getenv("GPT_API", "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")
 DEEP_API = getenv("DEEP_API", "c8e3d7fc-1f7e-455b-8019-5c1b7f21047a")
