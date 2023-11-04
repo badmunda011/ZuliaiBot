3d51ce781d3ad33f8fcc7c71821f62dc7c16cde4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-11-04 13:47:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "6196153811:AAGdVbn8tc6S2znNcOb7tWWoTxFzzZXbycE")
-POKI_TOKEN = getenv("BOT_TOKEN", "6763397502:AAGwKKYxTucW2POx6MYY_275FFo_y8oD_7k")
+POKI_TOKEN = getenv("POKI_TOKEN", "6763397502:AAGwKKYxTucW2POx6MYY_275FFo_y8oD_7k")
 BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
 GPT_API = getenv("GPT_API", "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")
