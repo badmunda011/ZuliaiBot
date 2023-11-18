89b9fad7ea49de2e969a56011128eb05fe506605
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-19 00:00:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "6196153811:AAFHdz8BypAWxPvkIw0h_dHtYIClZ0gXvwY")
-POKI_TOKEN = getenv("POKI_TOKEN", "6763397502:AAGwKKYxTucW2POx6MYY_275FFo_y8oD_7k")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+POKI_TOKEN = getenv("POKI_TOKEN", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
-GPT_API = getenv("GPT_API", "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")
-DEEP_API = getenv("DEEP_API", "c8e3d7fc-1f7e-455b-8019-5c1b7f21047a")
+GPT_API = getenv("GPT_API", "")
+DEEP_API = getenv("DEEP_API", "")
